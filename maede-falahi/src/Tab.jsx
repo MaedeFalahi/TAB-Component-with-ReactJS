1791de55fde113a3e7b6c064a429908eea8e1173
maede <fmaede71@gmail.com>
2024-08-25 11:51:34 +0330

    tab 4

--- maede-falahi/src/Tab.jsx
+++ maede-falahi/src/Tab.jsx
@@ -25,33 +25,42 @@ function Tab() {
          <div className={styles.container}>
       <ul className={styles.tabList}>
         <li
-          className={`styles.tabs ${getActiveClass(1, styles.activeTabs)}`}
+          className={`${styles.tabs} ${getActiveClass(1, styles.activeTabs)}`}
           onClick={() => toggleTab(1)}
         >
           Tab 1
         </li>
         <li
-          className={`styles.tabs ${getActiveClass(2, styles.activeTabs)}`}
+          className={`${styles.tabs} ${getActiveClass(2, styles.activeTabs)}`}
           onClick={() => toggleTab(2)}
         >
           Tab 2
         </li>
         <li
-          className={`styles.tabs ${getActiveClass(3, styles.activeTabs)}`}
+          className={`${styles.tabs} ${getActiveClass(3, styles.activeTabs)}`}
           onClick={() => toggleTab(3)}
         >
           Tab 3
         </li>
+        <li
+          className={`${styles.tabs} ${getActiveClass(4, styles.activeTabs)}`}
+          onClick={() => toggleTab(4)}
+        >
+          Tab 4
+        </li>
       </ul>
       <div className={styles.contentContainer}>
         <div className={toggleState === 1 ? `${getActiveClass(1, styles.activeContent )}`  : styles.content }>
-          <h2>Lorem</h2>
+          <h2>Lorem1 ipsum dolor sit amet, consectetur adipiscing elit</h2>
         </div>
         <div className={toggleState === 2 ? `${getActiveClass(2, styles.activeContent)}`  : styles.content }>
-          <h2>Ipsum</h2>
+          <h2>Lorem2 ipsum dolor sit amet, consectetur adipiscing elit</h2>
         </div>
         <div className={toggleState === 3 ? `${getActiveClass(3,  styles.activeContent)}` : styles.content }>
-          <h2>Dolor</h2>
+          <h2>Lorem3 ipsum dolor sit amet, consectetur adipiscing elit</h2>
+        </div>
+        <div className={toggleState === 4 ? `${getActiveClass(4,  styles.activeContent)}` : styles.content }>
+          <h2>Lorem4 ipsum dolor sit amet, consectetur adipiscing elit</h2>
         </div>
       </div>
     </div>
